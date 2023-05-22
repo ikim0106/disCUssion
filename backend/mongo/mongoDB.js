33dac4ead440d682c5e16d70573f50bd4fed17e7
ikim0106 <ikim0106@hotmail.com>
2023-05-22 21:52:26 +0900

    migration

--- backend/mongo/mongoDB.js
+++ backend/mongo/mongoDB.js
@@ -14,10 +14,8 @@ const mongoURI = config.mongoURI
 
 const connectToMongoDB = async () => {
    // console.log('mongoURI', mongoURI)
-   const debug = await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-   })
+   mongoose.set('strictQuery', true)
+   const debug = await mongoose.connect(mongoURI)
    const db = debug.connection
    db.on('error', ()=>{
       console.log('monkas mongo error')
